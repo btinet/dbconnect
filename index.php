005dea90ad61cf90b4ba99ee6ef7e647d0a2bd34
Benjamin Wagner <benjamin.voigt@vapita.de>
2022-02-02 20:59:16 +0100

    update demo application

--- index.php
+++ index.php
@@ -7,33 +7,78 @@ use Vapita\Model\Database;
 include('./vendor/autoload.php');
 
 // Neue Instanz der Datenbank-Klasse mit Verbindungsdaten erstellen. Ändere hier die Angaben entsprechend deiner Datenbank.
-$db = new Database(['host' => 'localhost:8889','dbname' => 'information_schema','charset' =>'utf8'],'root','root');
+$db = new Database(['host' => 'localhost:8889','dbname' => 'test','charset' =>'utf8'],'root','root');
 
-// Datensätze aus einer Tabelle abfragen. Hier wurde nach Datensätzen in der Tabelle "plugins" gesucht, die "binlog" im Feld "plugin_name" enthalten.
-$plugins = $db->select("SELECT * FROM plugins WHERE plugin_name = :plugin_name",['plugin_name' => 'binlog']);
 
 // Verkürzte Methode zum Abruf von mehreren Datensätzen anhand von Suchparametern.
-$pluginResults = $db->findBy('plugins',[
-    'plugin_version' => '1.0'
-]);
+$studentSingleResultById = $db->find('student',34);
+$studentResultsByValue = $db->findBy('student',['last_name' => 'Rölke']);
+$studentSingleResultByValue = $db->findOneBy('student',['first_name' => 'Anna']);
+$studentAll = $db->findAll('student',['last_name' => 'ASC', 'first_name' => 'ASC']);
 
-// Verkürzte Methode zum Abruf eines Datensatzes anhand von Suchparametern.
-$pluginResult = $db->findOneBy('plugins',[
-    'plugin_name' => 'binlog'
-]);
+
 
 // Datensätze formatiert ausgeben. Man könnte hier noch mit "if" prüfen, ob überhaupt Datensätze gefunden wurden.
-print('<h2>Benutzerdefinierte Abfrage</h2>');
-print('<pre><code>');
-print_r($plugins->fetchAll(3));
-print('</code></pre>');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-print('<h2>Mehrere Datensätze (findBy())</h2>');
-print('<pre><code>');
-print_r($pluginResults);
-print('</code></pre>');
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-print('<h2>Ein Datensatz (findOneBy())</h2>');
-print('<pre><code>');
-print_r($pluginResult);
-print('</code></pre>');
+    <title>Datenbank Tutorial</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">Datenbanken</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Abrufen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Bearbeiten</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Löschen</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container my-3">
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    Alle StudentInnen
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Test</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <?php foreach ($studentAll as $student) : ?>
+                        <li class="list-group-item"><?= $student->first_name . ' ' . $student->last_name ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
