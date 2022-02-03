7ed2584ef5fc0d8f80bf62ac7481d8b8c0391804
Benjamin Wagner <benjamin.voigt@vapita.de>
2022-02-03 15:22:42 +0100

    update styles

--- index.php
+++ index.php
@@ -76,6 +76,7 @@ $students = $db->findAll('student',['last_name' => 'ASC', 'first_name' => 'ASC']
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="./css/style.css" rel="stylesheet">
 
     <title>Datenbank Tutorial</title>
 </head>
