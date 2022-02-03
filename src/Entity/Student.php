e6655db7f642bd1b5221078e7b28fff443978abb
Benjamin Wagner <benjamin.voigt@vapita.de>
2022-02-03 06:06:07 +0100

    create entity class

---
+++ src/Entity/Student.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Vapita\Entity;
+
+class Student
+{
+
+    private int $id;
+
+    private string $firstName;
+
+    private string $lastName;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+}
